586a8dbcf0a028cebda51e8b62bb59159c528240
Alexander Couzens <lynxis@fe80.eu>
2016-11-12 21:04:18 +0100

    firmware: create new variable UPLOAD_DIR and remove /unstable/weekly/stable

--- freifunk-firmware.sh
+++ freifunk-firmware.sh
@@ -10,18 +10,9 @@ if [ -z "$NPROC" ] ; then
 	NPROC=$(grep -c "^processor" /proc/cpuinfo)
 fi
 
-if [ $BUILD_TYPE = "unstable" ] ; then
-	VERSION_REPO="http://buildbot.berlin.freifunk.net/buildbot/unstable/%V/%S/packages"
-	VERSION_NUMBER="$BUILD_NUMBER"
-	MAKE_OPTIONS="VERSION_REPO=$VERSION_REPO VERSION_NUMBER=$VERSION_NUMBER"
-elif [ $BUILD_TYPE = "weekly" ] ; then
-	VERSION_REPO="http://buildbot.berlin.freifunk.net/buildbot/weekly/%V/%S/packages"
-	VERSION_NUMBER="$BUILD_NUMBER"
-	MAKE_OPTIONS="VERSION_REPO=$VERSION_REPO VERSION_NUMBER=$VERSION_NUMBER"
-elif [ $BUILD_TYPE = "stable" ] ; then
-	VERSION_REPO="http://buildbot.berlin.freifunk.net/buildbot/stable/%V/%S/packages"
-	MAKE_OPTIONS="VERSION_REPO=$VERSION_REPO"
-fi
+VERSION_REPO="http://repo.fe80.eu/freifunk/${UPLOAD_DIR}/%V/%S/packages"
+VERSION_NUMBER="$BUILD_NUMBER"
+MAKE_OPTIONS="VERSION_REPO=$VERSION_REPO VERSION_NUMBER=$VERSION_NUMBER"
 
 make -j8 TARGET=${ARCH} BUILD_LOG=1 clean || true
 make -j8 TARGET=${ARCH} ${MAKE_OPTIONS} BUILD_LOG=1
